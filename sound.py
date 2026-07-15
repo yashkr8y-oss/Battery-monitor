9581e4d29eb58346709a28b13d775672939e9ccc
yashkr8y-oss <yashkr8.y@gmail.com>
2026-07-15 23:35:31 +0530

    Version 1.1 - Added sound alerts and debounce

--- sound.py
+++ sound.py
@@ -5,8 +5,14 @@ SOUND_FILE = os.path.join("assets", "sounds", "alert.wav")
 
 
 def play_alert():
+    print("Looking for:", SOUND_FILE)
+    print("Exists:", os.path.exists(SOUND_FILE))
+
     if os.path.exists(SOUND_FILE):
+        print("Playing sound...")
         winsound.PlaySound(
             SOUND_FILE,
             winsound.SND_FILENAME | winsound.SND_ASYNC
         )
+    else:
+        print("Alert sound not found.")
